56c67f5e533cf05eae739e3588eea003b29428e5
BOBaraki <bobosgithub@gmail.com>
2022-02-02 10:54:48 +0100

    initial commit for cloth state estimation and data labeling

---
+++ data_labeling.py
@@ -0,0 +1,109 @@
+import os
+from glob import glob
+
+import pandas as pd
+import numpy as np
+
+import csv
+
+import pdb
+
+PATH = "/home/tzortzis/real_data/Downloads/rectangular_brown_stripes/two_hand_side/"
+EXT = "*.csv"
+img_EXT = "*.png"
+fn = 'rect_brown_stripes_two_hand_side_'
+
+header = ['filename', 'cloth_state', 'false_labeling']
+
+# cloth_state = ['flat', 'semi-lifted-onehand', 'diagonally Folded']
+cloth_state = ['flat', 'semi-lifted-twohands', 'folded']
+# cloth_state = ['flat', 'semi-lifted-onehand', 'folded']
+# cloth_state = ['flat', 'semi-lifted-twohands-middle']
+
+
+flat = True
+semi = False
+folded = False
+
+
+def stateidentification(filename, flat, semi, folded):
+    if filename == fn+'100001' or filename == fn+'100120':
+        flat = True
+        semi = False
+        folded = False
+        # lifted = False
+    elif name == fn+'100036' or name == fn+'200045':
+        flat = False
+        semi = True
+        folded = False
+        # lifted = False
+    # elif name == 'one_hand_lifting_100089' or name == 'one_hand_lifting_200098':
+    #     flat = False
+    #     semi = False
+    #     semi_crampled = True
+    #     lifted = False
+    elif name == fn+'100095' or name == fn+'200105':
+        flat = False
+        semi = False
+        folded = True
+        # lifted = True
+    return flat, semi, folded
+
+
+# def stateidentification(filename, flat, semi, folded):
+#     if filename == fn+'100001' or filename == fn+'100122' or filename == fn+'200124':
+#         flat = True
+#         semi = False
+#         folded = False
+#         # lifted = False
+#     elif name == fn+'100035' or name == fn+'200042' or name == fn+'300054':
+#         flat = False
+#         semi = True
+#         folded = False
+#         # lifted = False
+#     # elif name == 'one_hand_lifting_100089' or name == 'one_hand_lifting_200098':
+#     #     flat = False
+#     #     semi = False
+#     #     semi_crampled = True
+#     #     lifted = False
+#     elif name == fn+'10008700' or name == fn+'200090000' or name == fn+'3000910000':
+#         flat = False
+#         semi = False
+#         folded = True
+#         # lifted = True
+#     return flat, semi, folded
+
+filenames = next(os.walk(PATH), (None, None, []))[2]
+
+# pdb.set_trace()
+flist = []
+for file in filenames:
+    flist.append(os.path.splitext(file)[0])
+
+
+flist.sort()
+data = []
+temp_cloth_state = cloth_state[0]
+for name in flist:
+    data.append([name, temp_cloth_state])
+    flat, semi, folded = stateidentification(name, flat, semi, folded)
+    if flat:
+        temp_cloth_state = cloth_state[0]
+    elif semi:
+        temp_cloth_state = cloth_state[1]
+    elif folded:
+        temp_cloth_state = cloth_state[2]
+    # else:
+    #     temp_cloth_state = cloth_state[3]
+
+# print(data)
+
+
+with open(PATH + 'data.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+
+    # write multiple rows
+    writer.writerows(data)
---
+++ grad_cam.py
@@ -0,0 +1,160 @@
+import argparse
+import torch.nn as nn
+import cv2
+import numpy as np
+import torch
+from torchvision import models
+from pytorch_grad_cam import GradCAM, \
+    ScoreCAM, \
+    GradCAMPlusPlus, \
+    AblationCAM, \
+    XGradCAM, \
+    EigenCAM, \
+    EigenGradCAM, \
+    LayerCAM, \
+    FullGrad
+from pytorch_grad_cam import GuidedBackpropReLUModel
+from pytorch_grad_cam.utils.image import show_cam_on_image, \
+    deprocess_image, \
+    preprocess_image, \
+    scale_cam_image
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use-cuda', action='store_true', default=False,
+                        help='Use NVIDIA GPU acceleration')
+    parser.add_argument(
+        '--image-path',
+        type=str,
+        default='/home/tzortzis/real_data/Downloads/rectangular_grid_sparse/one_hand_lift/rect_grid_sparse_one_hand_lift_100000.png',
+        help='Input image path')
+    parser.add_argument('--aug_smooth', action='store_true',
+                        help='Apply test time augmentation to smooth the CAM')
+    parser.add_argument(
+        '--eigen_smooth',
+        action='store_true',
+        help='Reduce noise by taking the first principle componenet'
+        'of cam_weights*activations')
+    parser.add_argument('--method', type=str, default='gradcam',
+                        choices=['gradcam', 'gradcam++',
+                                 'scorecam', 'xgradcam',
+                                 'ablationcam', 'eigencam',
+                                 'eigengradcam', 'layercam', 'fullgrad'],
+                        help='Can be gradcam/gradcam++/scorecam/xgradcam'
+                             '/ablationcam/eigencam/eigengradcam/layercam')
+
+    args = parser.parse_args()
+    args.use_cuda = args.use_cuda and torch.cuda.is_available()
+    if args.use_cuda:
+        print('Using GPU for acceleration')
+    else:
+        print('Using CPU for computation')
+
+    return args
+
+
+if __name__ == '__main__':
+    """ python cam.py -image-path <path_to_image>
+    Example usage of loading an image, and computing:
+        1. CAM
+        2. Guided Back Propagation
+        3. Combining both
+    """
+
+    args = get_args()
+    methods = \
+        {"gradcam": GradCAM,
+         "scorecam": ScoreCAM,
+         "gradcam++": GradCAMPlusPlus,
+         "ablationcam": AblationCAM,
+         "xgradcam": XGradCAM,
+         "eigencam": EigenCAM,
+         "eigengradcam": EigenGradCAM,
+         "layercam": LayerCAM,
+         "fullgrad": FullGrad}
+
+    # model = models.resnet50(pretrained=True)
+
+    from efficientnet_pytorch import EfficientNet
+
+    model_ft = EfficientNet.from_pretrained('efficientnet-b0')
+
+    for param in model_ft.parameters():
+        param.requires_grad = True  # By doing this I am keeping the parameters of the feature layers frozen so they won't update
+
+    num_fc_ftr = model_ft._fc.in_features
+    model_ft._fc = nn.Sequential(
+        nn.Linear(num_fc_ftr, 1000),
+        nn.BatchNorm1d(1000),
+        nn.ReLU(),
+        nn.Dropout(p=0.2),
+        nn.Linear(1000, 500),
+        nn.BatchNorm1d(500),
+        nn.ReLU(),
+        nn.Dropout(p=0.2),
+        nn.Linear(500, 10))
+
+    model_ft.load_state_dict(torch.load('/home/tzortzis/all_real_eff-finetuned-no-grid-sparse.pt'))
+    # Choose the target layer you want to compute the visualization for.
+    # Usually this will be the last convolutional layer in the model.
+    # Some common choices can be:
+    # Resnet18 and 50: model.layer4
+    # VGG, densenet161: model.features[-1]
+    # mnasnet1_0: model.layers[-1]
+    # You can print the model to help chose the layer
+    # You can pass a list with several target layers,
+    # in that case the CAMs will be computed per layer and then aggregated.
+    # You can also try selecting all layers of a certain type, with e.g:
+    # from pytorch_grad_cam.utils.find_layers import find_layer_types_recursive
+    # find_layer_types_recursive(model, [torch.nn.ReLU])
+    target_layers = [model_ft._conv_head]
+
+    rgb_img = cv2.imread(args.image_path, 1)[:, :, ::-1]
+    rgb_img = np.float32(rgb_img) / 255
+    input_tensor = preprocess_image(rgb_img,
+                                    mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
+
+
+    # We have to specify the target we want to generate
+    # the Class Activation Maps for.
+    # If targets is None, the highest scoring category (for every member in the batch) will be used.
+    # You can target specific categories by
+    # targets = [e.g ClassifierOutputTarget(281)]
+    targets = None
+
+    # Using the with statement ensures the context is freed, and you can
+    # recreate different CAM objects in a loop.
+    cam_algorithm = methods[args.method]
+    with cam_algorithm(model=model_ft,
+                       target_layers=target_layers,
+                       use_cuda=args.use_cuda) as cam:
+
+        # AblationCAM and ScoreCAM have batched implementations.
+        # You can override the internal batch size for faster computation.
+        cam.batch_size = 32
+        grayscale_cam = cam(input_tensor=input_tensor,
+                            targets=targets,
+                            aug_smooth=args.aug_smooth,
+                            eigen_smooth=args.eigen_smooth)
+
+        # Here grayscale_cam has only one image in the batch
+        grayscale_cam = grayscale_cam[0, :]
+
+        cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+
+        # cam_image is RGB encoded whereas "cv2.imwrite" requires BGR encoding.
+        cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
+
+    gb_model = GuidedBackpropReLUModel(model=model_ft, use_cuda=args.use_cuda)
+    gb = gb_model(input_tensor, target_category=None)
+
+    cam_mask = cv2.merge([grayscale_cam, grayscale_cam, grayscale_cam])
+    cam_gb = deprocess_image(cam_mask * gb)
+    gb = deprocess_image(gb)
+
+    cv2.imwrite(f'{args.method}_cam.jpg', cam_image)
+    cv2.imwrite(f'{args.method}_gb.jpg', gb)
+    cv2.imwrite(f'{args.method}_cam_gb.jpg', cam_gb)
